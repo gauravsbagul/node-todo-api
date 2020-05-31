d4c98599367370df52020ec0de340cebd147aa91
Gaurav Bagul <gauravsbagul@gmail.com>
2020-05-31 18:19:16 +0530

    Setup confg

---
+++ server/config/config.js
@@ -0,0 +1,11 @@
+/** @format */
+var env = process.env.NODE_ENV || "development";
+console.log("TCL::process.env.NODE_ENV env", env);
+
+if (env === "development") {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = "mongodb://localhost:27017/TodoApp";
+} else if (env === "test") {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = "mongodb://localhost:27017/TodoAppTest";
+}
--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 /** @format */
+
+require("./config/config");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const { ObjectID } = require("mongodb");
@@ -9,7 +12,7 @@ const { Todo } = require("./models/todo");
 const { User } = require("./models/user");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
