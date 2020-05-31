f482b8b94e83fb70a3c4387e6ba8b69d903ebebc
Gaurav Bagul <gauravsbagul@gmail.com>
2020-05-31 15:15:50 +0530

    Added test for Post API

---
+++ server/tests/server.test.js
@@ -0,0 +1,65 @@
+/** @format */
+
+const expect = require("expect");
+const request = require("superTest");
+
+const { app } = require("./../server");
+const { Todo } = require("./../models/todo");
+const { User } = require("./../models/user");
+
+beforeEach((done) => {
+  Todo.remove({}).then(() => done());
+});
+
+describe("POST /todos", () => {
+  it("should create a new todo", (done) => {
+    var text = "Test todo text";
+
+    request(app)
+      .post("/todos")
+      .send({
+        text,
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.text).toBe(text);
+      })
+      .end((err, res) => {
+        if (err) {
+          console.log("TCL::Error while saving todo err", err);
+          return done(err);
+        }
+
+        Todo.find()
+          .then((todos) => {
+            expect(todos.length).toBe(1);
+            expect(todos[0].text).toBe(text);
+            done();
+          })
+          .catch((err) => {
+            console.log("TCL:: err", err);
+            done(err);
+          });
+      });
+  });
+
+  it("should not create todo with invalid body data", (done) => {
+    request(app)
+      .post("/todos")
+      .send({})
+      .expect(400)
+      .end((err, res) => {
+        console.log("TCL:: res", res);
+        if (err) {
+          console.log("TCL:: err", err);
+          return done(err);
+        }
+        Todo.find()
+          .then((todos) => {
+            expect(todos.length).toBe(0);
+            done();
+          })
+          .catch((err) => done(err));
+      });
+  });
+});
