32e9617dbdefc966d52a53a730d071b0f9539c54
Gaurav Bagul <gauravsbagul@gmail.com>
2020-06-06 14:08:44 +0530

    Added private routes for all

--- server/models/todo.js
+++ server/models/todo.js
@@ -17,6 +17,10 @@ const Todo = mongoose.model("Todo", {
     type: Number,
     default: null,
   },
+  _creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    require: true,
+  },
 });
 
 module.exports = { Todo };
--- server/server.js
+++ server/server.js
@@ -16,9 +16,10 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
-app.post("/todos", (req, res) => {
+app.post("/todos", authenticate, (req, res) => {
   var todo = new Todo({
     text: req.body.text,
+    _creator: req.user._id,
   });
 
   todo.save().then(
@@ -31,8 +32,10 @@ app.post("/todos", (req, res) => {
   );
 });
 
-app.get("/todos", (req, res) => {
-  Todo.find().then(
+app.get("/todos", authenticate, (req, res) => {
+  Todo.find({
+    _creator: req.user._id,
+  }).then(
     (todos) => {
       res.send({ todos });
     },
@@ -42,14 +45,15 @@ app.get("/todos", (req, res) => {
   );
 });
 
-app.get("/todo/:id", (req, res) => {
+app.get("/todo/:id", authenticate, (req, res) => {
   const { id } = req.params;
   if (!ObjectID.isValid(id)) {
     return res.status(404).send({ error: "Not a valid id" });
   }
 
-  Todo.findById({
+  Todo.findOne({
     _id: id,
+    _creator: req.user._id,
   }).then(
     (todo) => {
       if (!todo) {
@@ -63,7 +67,7 @@ app.get("/todo/:id", (req, res) => {
   );
 });
 
-app.delete("/todo/:id", (req, res) => {
+app.delete("/todo/:id", authenticate, (req, res) => {
   const { id } = req.params;
   if (!ObjectID.isValid(id)) {
     return res.status(404).send({ error: "Not a valid id" });
@@ -71,6 +75,7 @@ app.delete("/todo/:id", (req, res) => {
 
   Todo.findByIdAndRemove({
     _id: id,
+    _creator: req.user._id,
   }).then(
     (todo) => {
       if (!todo) {
@@ -86,7 +91,7 @@ app.delete("/todo/:id", (req, res) => {
   );
 });
 
-app.patch("/todo/:id", (req, res) => {
+app.patch("/todo/:id", authenticate, (req, res) => {
   const { id } = req.params;
 
   var body = _.pick(req.body, ["text", "completed"]);
@@ -101,7 +106,11 @@ app.patch("/todo/:id", (req, res) => {
     body.completedAt = null;
   }
 
-  Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then(
+  Todo.findOneAndUpdate(
+    { _id: id, _creator: req.user._id },
+    { $set: body },
+    { new: true }
+  ).then(
     (todo) => {
       if (!todo) {
         return res.status(404).send({ message: "Todo not found" });
